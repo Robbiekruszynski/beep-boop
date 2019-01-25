0cf995a7309cc6976881725edef268f3aad41c01
Student <robbiekruszynski@gmail.com>
2019-01-25 11:35:41 -0800

    add isNaN to prevent non number entry, continue to work on div logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,18 @@ var numberOutput = function(userNumber) {
   // var dumpOldArray = [];
   var printOut = [];
 
-  // if(isNaN(userNumber) {
-  //   return "Please enter a number"
-  // }
+  if(isNaN(userNumber)) {
+    return "Please enter a number"
+  }
 
 //creating a for loop to count by 1 up to userNumber
   for (var i =0; i <=userNumber; i++) {
-    //using i.toString capturing the i and converting to strring to find "3" "2" "1"
-    if(i.toString().includes("3")){
+  //using i.toString capturing the i and converting to strring to find "3" "2" "1"
+
+  if(i%5===0){
+    printOut.push("Test")
+
+    }else if(i.toString().includes("3")){
     printOut.push("I'm sorry, Dave. I'm afraid I can't do that.")
     //Using .includes() method to determines whether an array includes targetted value among its entries.
     }else if (i.toString().includes("2")) {
@@ -30,22 +34,14 @@ var numberOutput = function(userNumber) {
   }
     return printOut;
 };
-
-// var MathOutput = function(userMathNumber){
-//   var printMath = [];
-//
-//   for (var = 0; i<=userMathNumber; i++){
-//     if
-//   }
-//
-// });
-
 // user-interface logic
   $(document).ready(function(){
   $("form#manipulator").submit(function(event){
   event.preventDefault();
+
   var userInput = $("input#userInput").val();
 
   $("#yourResult").text(numberOutput(userInput));
+
 })
 });
