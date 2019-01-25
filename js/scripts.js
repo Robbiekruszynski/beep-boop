0bbb97dcf6042275859ab5098502ee204f49a7de
Student <example@example.com>
2019-01-25 08:42:33 -0800

    start logic buildout for beep, attempt to use .includes()

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//business logic
+
+var numberOutput = function(userNumber) {
+  var dumpOldArray = [];
+  var dumpNewArray = [];
+
+//creating a for loop to count by 1 up to userNumber
+  for (var i =0; i <=userNumber; i++) {
+  dumpOldArray.push(i+1);
+  }
+};
+
+  for (var = 0; i <=userNumber; i++) {
+    if (dumpOldArray[i].includes(1)){
+      dumpNewArray[i] = "Beep"
+        }
+  }
+
+
+
+
+// user-interface logic
