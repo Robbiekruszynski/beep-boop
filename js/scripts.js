ca063f59ceb8bf96dd24851777ba08854c7643d9
Student <example@example.com>
2019-01-25 09:01:41 -0800

    rework logic to use the string information without parseInt to capture 1 2 3 opposed to 1, 2, 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
 //business logic
 
+//using elements within the array as a string to search for targetted value
+//(1,2,3) without converting with parseInt
+
 var numberOutput = function(userNumber) {
   var dumpOldArray = [];
   var dumpNewArray = [];
 
 //creating a for loop to count by 1 up to userNumber
   for (var i =0; i <=userNumber; i++) {
-  dumpOldArray.push(i+1);
-  }
+
+    if(i.toString().includes("1")){
+    } dumpNewArray.push("Beep");
+
+    
 
 
-  for (var i = 0; i <=userNumber; i++) {
-    if (dumpOldArray[i].includes(1)){
-      dumpNewArray[i] = "Beep"
-        }
+  // for (var i = 0; i <=userNumber; i++) {
+  //
+  //   if (dumpOldArray[i].includes(1)){
+  //     dumpNewArray[i] = "Beep";
+  //     console.log(userNumber)
+  //
+  //  }else if (dumpOldArray[i].includes(2)){
+  //     dumpNewArray[i] = "Boop";
+  //
+  //  }else if (dumpOldArray[i].includes(3)){
+  //     dumpNewArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+  //  }
+  //
+
   }
 
 };
